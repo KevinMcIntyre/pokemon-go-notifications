773afdd4714585b0206f7866d70d2ab22db5ed02
piotrromanowski <piotrromanowski1@gmail.com>
2016-07-23 11:26:34 -0400

    enable/disable button according to the validation

--- app/scripts.babel/popup.js
+++ app/scripts.babel/popup.js
@@ -1,42 +1,47 @@
 'use strict';
 
+// Declare varibales and set placeholders
 const submitButton = document.getElementById('coordinate_submit');
 const longitudeInput = document.getElementById('longitude');
 const latitudeInput = document.getElementById('latitude');
-
 const LongitudeErrorText = document.getElementById('invalid_longitude');
 const LatitudeErrorText = document.getElementById('invalid_latitude');
+
+let isLatitudeValid = false;
+let isLongitutdeValid = false;
 
 longitudeInput.placeholder = localStorage['longitude'];
 latitudeInput.placeholder = localStorage['latitude'];
 
 // Event Handlers
 submitButton.onclick = function() {
-  const isInvalid = isNaN(latitudeInput.value) || isNaN(longitudeInput.value);
+  const isValid = isLatitudeValid && isLongitutdeValid;
 
-  if (!isInvalid) {
+  if (isValid) {
     localStorage['latitude'] = latitudeInput.value;
     localStorage['longitude'] = longitudeInput.value;
     longitudeInput.placeholder = longitudeInput.value;
     LatitudeErrorText.placeholder = latitudeInput.value;
-    chrome.runtime.sendMessage({repoll: true}, function(response) { });
+    chrome.runtime.sendMessage({ repoll: true }, function(response) { });
     window.close();
   }
 }
 
 latitudeInput.onchange = function(e) {
   const value = e.target.value;
-  isValidLatitude(value);
+  isLatitudeValid = isValidLatitude(value);
+  setButtonState();
 }
 
 longitudeInput.onchange = function(e) {
   const value = e.target.value;
-  isValidLongitude(value);
+  isLongitutdeValid = isValidLongitude(value);
+  setButtonState();
 }
 
 // Validation for the inputs
 function isValidLatitude(value) {
-  if (isNaN(value) || (value > 90) || (value < -90)) {
+  if (!value || isNaN(value) || (value > 90) || (value < -90)) {
     LatitudeErrorText.style.display = '';
     return false;
   } else {
@@ -46,13 +51,17 @@ function isValidLatitude(value) {
 }
 
 function isValidLongitude(value) {
-  if (isNaN(value) || (value > 180) || (value < -180)) {
+  if (!value || isNaN(value) || (value > 180) || (value < -180)) {
     LongitudeErrorText.style.display = '';
     return false;
   } else {
     LongitudeErrorText.style.display = 'none';
     return true;
   }
+}
+
+function setButtonState() {
+  submitButton.disabled = !(isLatitudeValid && isLongitutdeValid);
 }
 
 // Update Current Pokemon List
